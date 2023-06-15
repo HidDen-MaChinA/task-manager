3e03b04d24ec18f71472ca9bf6e1189400ce76b0
hidden-machina <hei.franco.3@gmail.com>
2023-06-15 11:33:28 +0300

    feat:add useLocalStorage

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,5 +1,18 @@
-const useLocalStorage = () => {
+"use client"
 
+const useLocalStorage = (key:string,initialValue?:string) => {
+  localStorage.setItem(key,initialValue ? initialValue:"");
+  const setValue = (str:string)=>{
+    localStorage.setItem(key,str)
+  }
+
+  const getValue = ()=>{
+    localStorage.getItem(key)
+  }
+  return {
+    setValue:setValue,
+    getValue:getValue
+  }
 }
 
 export {
